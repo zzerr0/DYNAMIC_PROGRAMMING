5a3b6ced3ceec5a87e01f41c67d50f6af704d535
Shivam Yadav <38142513+zzerr0@users.noreply.github.com>
2020-05-30 11:31:36 +0530

    Create FibonacciNumber_Method2.cpp

---
+++ FibonacciNumber_Method2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int fib(int n)
+{
+  unsigned f[n+2] ;
+  f[0]=0;
+  f[1]=1;
+  cout<<f[0]<<" "<<f[1]<<" ";
+  for(int i=2;i<=n;i++)
+  {
+    f[i]=f[i-1]+f[i-2];
+    cout<<f[i]<<" ";
+  }
+  cout<<endl;
+//return f[n]; to return nth element
+}
+int main()
+{
+ 
+cout<<"Enter the index of Fibonacci number"
+  <<" \n you want to find "<<endl;
+int n;
+cin>>n;
+//cout<<fib(n); it is used to get nth element
+fib(n);
+
+ 
+ return 0;
+}
